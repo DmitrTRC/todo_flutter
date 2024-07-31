e243f4a7fadb57182113130ade000126a2cb473b
Dmitry Morozov <9984398@gmail.com>
2024-07-31 22:59:55 +0300

    -> Demo . Try it!

---
+++ lib/demo.dart
@@ -0,0 +1,16 @@
+bool isProduction = false;
+
+void main() {
+  // final l1 = [1, 2, 3,
+  //   for (int i = 4; i < 10; i++) i,
+  //   if ( isProduction) 55,
+  //   10];
+  //
+  // print(l1);
+  var a = 10;
+  var b = 5;
+
+  var c = a / b; // Very interesting try this on dartpad.dev
+
+  print(c.runtimeType);
+}
