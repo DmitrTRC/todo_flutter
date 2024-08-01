fda81b6c82e4cbf0639bd86638f76fe771570d40
Dmitry Morozov <9984398@gmail.com>
2024-08-02 01:57:56 +0300

    NC

---
+++ lib/local_db.dart
@@ -0,0 +1,25 @@
+import 'weaponModel.dart';
+
+var weapon1 = Weapon(
+    'Blaser',
+    'R8',
+    '300WM',
+    11111111);
+
+var weapon2 = Weapon(
+    'Glock',
+    'G19',
+    '9mm',
+    22222222);
+
+var weapon3 = Weapon(
+    'AK-47',
+    'Kalashnikov',
+    '7.62mm',
+    33333333);
+
+var weapon4 = Weapon(
+    'M16',
+    'Colt',
+    '5.56mm',
+    44444444);
--- lib/weapon_list_view.dart
+++ lib/weapon_list_view.dart
@@ -1,29 +1,7 @@
 import 'package:flutter/material.dart';
-import 'weaponModel.dart';
 
-var weapon1 = Weapon(
-    'Blaser',
-    'R8',
-    '300WM',
-    11111111);
+import 'local_db.dart';
 
-var weapon2 = Weapon(
-    'Glock',
-    'G19',
-    '9mm',
-    22222222);
-
-var weapon3 = Weapon(
-    'AK-47',
-    'Kalashnikov',
-    '7.62mm',
-    33333333);
-
-var weapon4 = Weapon(
-    'M16',
-    'Colt',
-    '5.56mm',
-    44444444);
 
 final demoWeaponList = [
   weapon1,
