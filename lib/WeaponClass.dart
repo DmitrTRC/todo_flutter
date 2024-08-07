451bb5792c2513c4d1b92141ec76f1ab3345e677
Arina Morozova <arinagekkbrk@mail.ru>
2024-08-07 15:23:45 +0200

    homeqork

---
+++ lib/WeaponClass.dart
@@ -0,0 +1,8 @@
+
+class Weapon{
+  String name;
+  String range;
+  String damage;
+  Weapon(this.name, this.range, this.damage);
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'WeaponClass.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -11,8 +11,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: _title,
       theme: ThemeData(
+        scaffoldBackgroundColor: Colors.black87,
         // useMaterial3: false,
         primarySwatch: Colors.blue,
+        textTheme: const TextTheme(
+          bodyMedium: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            fontSize:40,
+          ),
+          labelSmall: TextStyle(
+            color: Colors.white38,
+            fontWeight: FontWeight.w700,
+            fontSize: 20,
+          ),
+        ),
+
       ),
       home: const MyHomePage(),
     );
@@ -56,6 +70,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final theme=Theme.of(context);
+    Weapon one = Weapon('Aka', '50', '1');
+    Weapon two = Weapon('M', '30', '15');
+    Weapon three = Weapon('HK', '60', '1');
+    Weapon four = Weapon('P90', '40', '2');
+    Weapon five = Weapon('M6', '55', '20');
+    Weapon six = Weapon('M6', '55', '20');
+    Weapon seven = Weapon('M6', '55', '20');
+    Weapon eight  = Weapon('M6', '55', '20');
+
+    final  Weaponlist = [one, two, three, four, five,six,seven,eight];
+
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -66,31 +92,23 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
-          title: const Text('Flutter Demo Click Counter'),
+          title: const Text('WeaponList'),
+          backgroundColor: Colors.amber,
+
         ),
-        body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: const TextStyle(fontSize: 25),
-              ),
-            ],
+        body:ListView.builder(
+          itemCount: 8,
+          itemBuilder:( BuildContext ,int i)=>ListTile(
+          title: Text(Weaponlist[i].name,
+            style: theme.textTheme.bodyMedium,
+
+          )
+            ,
+            subtitle: Text(Weaponlist[i].damage,
+                style: theme.textTheme.labelSmall,
+
+            ),
+
           ),
         ),
         floatingActionButton: Column(
